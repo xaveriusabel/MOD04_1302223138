8978b4e1256d92fb4d9e4800240e71d05e33119d
xaveriusabel <xaveriusabel@hmail.com>
2024-03-13 08:18:42 +0700

    Buah

--- MOD04_1302223138/Program.cs
+++ MOD04_1302223138/Program.cs
@@ -1,10 +1,73 @@
 ﻿namespace MOD04_1302223138
 {
-    internal class Program
+    public class KodeBuah
     {
-        static void Main(string[] args)
+
+        public enum Buah { Apel, Aprikot, Alpukat, Pisang, Paprika, Blackberry, Ceri, Kelapa, Jagung, Kurma, Durian, Anggur, Melon, Semangka };
+
+        public static string getKode(Buah buah)
         {
-            Console.WriteLine("Hello, World!");
+            string[] kodeBuah = { "A00", "B00", "C00", "D00", "E00", "F00", "G00", "H00", "I00", "J00", "K00", "L00", "M00", "N00" , "O00" };
+
+
+            return kodeBuah[(int)buah];
         }
+
+        static void Main(string[] agrs)
+        {
+            Buah buah = new Buah();
+            Console.WriteLine("Nama Buah : " + Buah.Apel);
+            Console.WriteLine("Kode Buah : " + getKode(Buah.Apel));
+            Console.WriteLine("Nama Buah : " + Buah.Aprikot);
+            Console.WriteLine("Kode Buah : " + getKode(Buah.Aprikot));
+            Console.WriteLine("Nama Buah : " + Buah.Alpukat);
+            Console.WriteLine("Kode Buah : " + getKode(Buah.Alpukat));
+            Console.WriteLine("Nama Buah : " + Buah.Pisang);
+            Console.WriteLine("Kode Buah : " + getKode(Buah.Pisang));
+            Console.WriteLine("Nama Buah : " + Buah.Paprika);
+            Console.WriteLine("Kode Buah : " + getKode(Buah.Paprika));
+            Console.WriteLine("Nama Buah : " + Buah.Blackberry);
+            Console.WriteLine("Kode Buah : " + getKode(Buah.Blackberry));
+            Console.WriteLine("Nama Buah : " + Buah.Ceri);
+            Console.WriteLine("Kode Buah : " + getKode(Buah.Ceri));
+            Console.WriteLine("Nama Buah : " + Buah.Kelapa);
+            Console.WriteLine("Kode Buah : " + getKode(Buah.Kelapa));
+            Console.WriteLine("Nama Buah : " + Buah.Jagung);
+            Console.WriteLine("Kode Buah : " + getKode(Buah.Jagung));
+            Console.WriteLine("Nama Buah : " + Buah.Kurma);
+            Console.WriteLine("Kode Buah : " + getKode(Buah.Kurma));
+            Console.WriteLine("Nama Buah : " + Buah.Durian);
+            Console.WriteLine("Kode Buah : " + getKode(Buah.Durian));
+            Console.WriteLine("Nama Buah : " + Buah.Anggur);
+            Console.WriteLine("Kode Buah : " + getKode(Buah.Anggur));
+            Console.WriteLine("Nama Buah : " + Buah.Melon);
+            Console.WriteLine("Kode Buah : " + getKode(Buah.Melon));
+            Console.WriteLine("Nama Buah : " + Buah.Semangka);
+            Console.WriteLine("Kode Buah : " + getKode(Buah.Semangka));
+
+        }
+
     }
 }
+public class posisiKarakterGame
+{
+    public enum gameState { Berdiri, Jongkok, Terbang, Tengkurap };
+
+    public enum trigger { tombolW, tombolS, tombolX };
+
+    static void Main(string[] agrs)
+    {
+        posisiKarakterGame.gameState kondisi = posisiKarakterGame.gameState.Berdiri;
+        Console.WriteLine("Masukan Misi : ");
+        string input = Console.ReadLine();
+        while (input != "udahan") ;
+        {
+            switch (kondisi)
+            {
+                case gameState.Berdiri:
+                    if (input == "S")
+                    } Console.WriteLine("Tombol Arah Bawah Ditekan")
+                      kondisi = gameState.Jongkok;
+            }
+        }
+    }
--- MOD04_1302223138/obj/Debug/net8.0/MOD04_1302223138.AssemblyInfo.cs
+++ MOD04_1302223138/obj/Debug/net8.0/MOD04_1302223138.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MOD04_1302223138")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f42bad734185a362ac13b45d8a4fbd14f69f20ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("MOD04_1302223138")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MOD04_1302223138")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
