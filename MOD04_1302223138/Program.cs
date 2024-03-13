f42bad734185a362ac13b45d8a4fbd14f69f20ca
xaveriusabel <xaveriusabel@gmail.com>
2024-03-13 07:05:06 +0700

    first commit

---
+++ MOD04_1302223138/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MOD04_1302223138
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
